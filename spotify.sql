9433f9a31ec08683c306537de9450858d5ef56f1
Puja Rai <pujarai7000@gmail.com>
2026-06-22 18:15:19 -0500

    Add advanced GROUP BY and ORDER BY queries

--- spotify.sql
+++ spotify.sql
@@ -44,3 +44,37 @@ Select  c14 as liked_status,
        AVG(CAST (c1 as real) )as avg_danceability
 From data 
 GROUP BY c14;
+
+--What is the average energy for liked vs disliked songs?DELETE FROM data
+DELETE FROM data
+WHERE c14='liked';
+SELECT AVG(c2) as avg_energy ,c14 as liked_status
+From data
+GROUP by c14;
+
+--Which time signature appears most frequently?
+SELECT COUNT (* ) as frequency,c13 as time_signature
+From data
+GROUP by c13
+ORDER BY frequency DESC
+LIMIT 1;
+
+
+--Which key has the highest average energy?
+SELECT AVG(c2) as avg_energy, c3 as keys
+From data
+GROUP BY c3
+ORDER BY  avg_energy DESC
+LIMIT 1;
+
+--Top 10 highest energy values in the dataset
+SELECT c2 as energy
+From data
+ORDER BY energy DESC
+LIMIT 10;
+
+--Show 10 slowest songs by tempo
+SELECT CAST(c11 as real) as tempo
+From data
+ORDER BY tempo asc
+LIMIT 10;
